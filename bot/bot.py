35054a4dad6b6157ed5645f15baa90aa82e29c42
roy <57131717+roypu1998@users.noreply.github.com>
2024-11-21 15:24:06 +0200

    Update bot.py V3

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,5 @@
 from telebot import TeleBot
-from utils.declear import token, flat_buy_eth, first_deposit, ethereum_price, my_balance_usd, my_balance_ils, get_profit, get_loss
+from utils.declear import token, flat_buy_eth, first_deposit, ethereum_price, my_balance_usd, my_balance_ils, get_profit, get_loss, ILS, USD
 
 bot = TeleBot(token=token)
 
@@ -9,8 +9,8 @@ def send_about(msg):
 
 @bot.message_handler(commands=['calc'])
 def send_about(msg):
-    msg_bck = (f'The price of Ethereum in USD is {ethereum_price['usd']}$'
-               f'\nThe price of Ethereum in ILS is {ethereum_price['ils']}₪'
+    msg_bck = (f'The price of Ethereum in USD is {ethereum_price[USD]}$'
+               f'\nThe price of Ethereum in ILS is {ethereum_price[ILS]}₪'
                f'\nYour crypto flat buy is: {flat_buy_eth} ETH and {my_balance_usd}$ and {my_balance_ils}₪')
     bot.send_message(chat_id=msg.chat.id, text=f'{msg_bck}')
     if my_balance_ils > first_deposit:
@@ -26,5 +26,3 @@ def send_about(msg):
 def run():
     print("bot start")
     bot.infinity_polling()
-
-
