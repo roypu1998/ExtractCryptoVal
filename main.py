bf1a4ad261eb3985612fb7439d50b23376ff7b81
roy <57131717+roypu1998@users.noreply.github.com>
2024-11-21 16:12:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from utils.declear import chat_id, my_balance_ils, get_profit, first_deposit
-from bot.bot import run, bot
+from bot.bot import thread, bot
 from time import sleep
-import threading
+
 
 if __name__ == '__main__':
-    thread = threading.Thread(target=run)
-    thread.start()
     while True:
         if my_balance_ils > first_deposit:
             bot.send_message(chat_id=chat_id, text=f'{get_profit(my_balance_ils, first_deposit)}')
