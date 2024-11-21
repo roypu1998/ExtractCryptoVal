3c493871c55d75a7ae7b82920319acec2a52ccdc
roy <57131717+roypu1998@users.noreply.github.com>
2024-11-21 15:16:39 +0200

    Update declear.py

--- utils/declear.py
+++ utils/declear.py
@@ -1,4 +1,4 @@
-from utils.utils import *
+from utils.utils import get_crypto_price, get_dollar_rate, get_profit, get_loss
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +10,6 @@ first_deposit = float(os.getenv('FIRST_DEPOSIT'))
 chat_id = int(os.getenv('CHAT_ID'))
 
 # calculate basic data
-ethereum_price = get_crypto_price('ethereum')
-my_balance_usd = flat_buy_eth * ethereum_price['usd']
-my_balance_ils = my_balance_usd * get_dollar_rate('ILS')
+ethereum_price = get_crypto_price("ethereum")
+my_balance_usd = flat_buy_eth * ethereum_price["usd"]
+my_balance_ils = my_balance_usd * get_dollar_rate("ILS")
