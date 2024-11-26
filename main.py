9b8b83473249260cf28f2080284760bec226ed21
roy <57131717+roypu1998@users.noreply.github.com>
2024-11-26 15:24:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from utils.declear import chat_id, my_balance_ils, get_profit, first_deposit
+from utils.declear import chat_id, my_balance_ils, get_profit, first_deposit, WEBHOOK_URL
 from bot.bot import bot
+from flask import Flask, request
 from time import sleep
-from flask import Flask, render_template
 import threading
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "i'm alive", 200
 
-def run_bot():
-    print("bot start")
-    bot.infinity_polling()
+# הגדרת webhook
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = request.get_json()
+    if update:
+        bot.process_new_updates([update])
+    return "OK"
+
+
+def run_flask():
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+
 
 def run_main():
     print('main start')
@@ -21,10 +28,15 @@ def run_main():
             bot.send_message(chat_id=chat_id, text=f'{get_profit(my_balance_ils, first_deposit)}')
         sleep(5)
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    thread_bot = threading.Thread(target=run_bot)
-    sleep(5)
-    thread_main = threading.Thread(target=run_main)
-    thread_bot.start()
-    thread_main.start()
+    # הגדרת webhook עם Telegram
+    bot.remove_webhook()
+    bot.set_webhook(url=f"{WEBHOOK_URL}/webhook")
+
+    # הרצת Flask ו-run_main באותה זמן באמצעות threading
+    flask_thread = threading.Thread(target=run_flask)
+    main_thread = threading.Thread(target=run_main)
+
+    flask_thread.start()
+    main_thread.start()
